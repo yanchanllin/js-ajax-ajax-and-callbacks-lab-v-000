75a41fddbe1f2c8a3b1cf663d2756c1e503eefd5
YanC Lin <yonalin1@gmail.com>
2018-09-13 15:36:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-$(document).ready(function (){
-});
+function searchRepositories() {
+	const searchTerms = $('#searchTerms').val();
+	$.get(`https://api.github.com/search/repositories?q=${searchTerms}`, 
+		function(data) {
+		$('#results').html(displayRepositories(data));
+	}).fail(error => {
+		displayError()
+	});
+}
